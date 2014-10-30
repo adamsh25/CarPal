05327a7c3b91c48a9132b8c0e44c7ca5ca7ffb30
Barry.Z <freestyle3@gmail.com>
2014-10-30 09:53:01 +0200

    Added main screen

---
+++ src/com/BooYa/CarPal/PendingRequest.java
@@ -0,0 +1,33 @@
+package com.BooYa.CarPal;
+
+/**
+ * Created by Barry.Z on 10/29/2014.
+ */
+public class PendingRequest
+{
+    private int request_id;
+    private String pending_request_info;
+
+
+    public PendingRequest(int request_id,String pending_request_info)
+    {
+        this.setRequest_id(request_id);
+        this.setPending_request_info(pending_request_info);
+    }
+
+    public int getRequest_id() {
+        return request_id;
+    }
+
+    public void setRequest_id(int request_id) {
+        this.request_id = request_id;
+    }
+
+    public String getPending_request_info() {
+        return pending_request_info;
+    }
+
+    public void setPending_request_info(String pending_request_info) {
+        this.pending_request_info = pending_request_info;
+    }
+}
