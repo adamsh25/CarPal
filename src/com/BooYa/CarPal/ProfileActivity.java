7794e6a4bf9681cf755d70039086835e86cc8557
Rony Lutsky <ronlut@gmail.com>
2014-10-30 16:41:36 +0200

    Added dependencies

--- src/com/BooYa/CarPal/ProfileActivity.java
+++ src/com/BooYa/CarPal/ProfileActivity.java
@@ -2,14 +2,29 @@ package com.BooYa.CarPal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * Created by beeri on 29/10/2014.
- */
 public class ProfileActivity extends Activity {
+    // Values for email and password at the time of the login attempt.
+    private String mEmail;
+    private String mPassword;
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private View mLoginFormView;
+    private View mLoginStatusView;
+    private TextView mLoginStatusMessageView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void Save() {
+
     }
 }
