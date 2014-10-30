c69d513f44aae01ad9995e8f9d55068ea4b97f73
Rony Lutsky <ronlut@gmail.com>
2014-10-31 01:34:02 +0200

    Added streets classes

---
+++ src/com/BooYa/CarPal/Street.java
@@ -0,0 +1,19 @@
+package com.BooYa.CarPal;
+
+/**
+ * Created by Rony on 31/10/2014.
+ */
+public class Street {
+    private int house_num;
+    private int street_code;
+    private String street_name;
+    private String street_name_eng;
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+}
