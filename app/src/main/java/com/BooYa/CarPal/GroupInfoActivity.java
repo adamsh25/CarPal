9d77c5c882658b03f0af09eb2df1eb48b6048b3e
Rony Lutsky <ronlut@gmail.com>
2014-11-29 21:31:19 +0200

    Added action bar to group pages

--- app/src/main/java/com/BooYa/CarPal/GroupInfoActivity.java
+++ app/src/main/java/com/BooYa/CarPal/GroupInfoActivity.java
@@ -1,21 +1,37 @@
 package com.BooYa.CarPal;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
+import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
 import com.pkmmte.view.CircularImageView;
 
 import java.util.*;
 
 
 public class GroupInfoActivity extends Activity implements View.OnClickListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerArrowDrawable drawerArrow;
 
 
     private Map<String,Map<String,View>> _dicViews;
@@ -26,7 +42,85 @@ public class GroupInfoActivity extends Activity implements View.OnClickListener 
         setContentView(R.layout.activity_group_info);
         Initiallize();
 
+        InitializeActionBar();
+        InitializeDrawer();
+    }
 
+    private void InitializeDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+
+        drawerArrow = new DrawerArrowDrawable(this) {
+            @Override
+            public boolean isLayoutRtl() {
+                return false;
+            }
+        };
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                drawerArrow, R.string.drawer_open,
+                R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+
+        String[] values = new String[]{
+                "PROFILE",
+                "MY RIDE GROUP",
+                "NOTIFICATIONS",
+                "ABOUT"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        //drawerArrow.setProgress(1f);
+                        mDrawerToggle.syncState();
+                        startActivity(new Intent(getBaseContext(), ProfileActivity.class));
+                        break;
+                    case 1:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        //drawerArrow.setProgress(0f);
+                        mDrawerToggle.syncState();
+                        startActivity(new Intent(getBaseContext(), GroupInfoActivity.class));
+                        break;
+                    case 2:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        mDrawerToggle.syncState();
+                        break;
+                    case 3:
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com/AppCarPal"));
+                        startActivity(browserIntent);
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private void InitializeActionBar() {
+        ActionBar ab = getActionBar();
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        ab.setCustomView(R.layout.action_bar_tab);
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+        ab.setTitle("MAROON 5");
     }
 
 
@@ -34,17 +128,20 @@ public class GroupInfoActivity extends Activity implements View.OnClickListener 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.group_info, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_car_pal_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                mDrawerLayout.closeDrawer(mDrawerList);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
         }
         return super.onOptionsItemSelected(item);
     }
@@ -301,5 +398,19 @@ public class GroupInfoActivity extends Activity implements View.OnClickListener 
         {
             editTextInANewWindow(v);
         }
+    }
+
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
--- app/src/main/java/com/BooYa/CarPal/GroupMapActivity.java
+++ app/src/main/java/com/BooYa/CarPal/GroupMapActivity.java
@@ -1,19 +1,37 @@
 package com.BooYa.CarPal;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.ikimuhendis.ldrawer.ActionBarDrawerToggle;
+import com.ikimuhendis.ldrawer.DrawerArrowDrawable;
 
 
 public class GroupMapActivity extends Activity {
     private GoogleMap map;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerArrowDrawable drawerArrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,25 +51,121 @@ public class GroupMapActivity extends Activity {
                     .icon(BitmapDescriptorFactory
                             .fromResource(R.drawable.ic_launcher)));
         }
+
+        InitializeActionBar();
+        InitializeDrawer();
+    }
+
+
+    private void InitializeDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+
+        drawerArrow = new DrawerArrowDrawable(this) {
+            @Override
+            public boolean isLayoutRtl() {
+                return false;
+            }
+        };
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                drawerArrow, R.string.drawer_open,
+                R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+
+        String[] values = new String[]{
+                "PROFILE",
+                "MY RIDE GROUP",
+                "NOTIFICATIONS",
+                "ABOUT"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        //drawerArrow.setProgress(1f);
+                        mDrawerToggle.syncState();
+                        startActivity(new Intent(getBaseContext(), ProfileActivity.class));
+                        break;
+                    case 1:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        //drawerArrow.setProgress(0f);
+                        mDrawerToggle.syncState();
+                        startActivity(new Intent(getBaseContext(), GroupInfoActivity.class));
+                        break;
+                    case 2:
+                        mDrawerToggle.setAnimateEnabled(true);
+                        mDrawerToggle.syncState();
+                        break;
+                    case 3:
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://facebook.com/AppCarPal"));
+                        startActivity(browserIntent);
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private void InitializeActionBar() {
+        ActionBar ab = getActionBar();
+        ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        ab.setCustomView(R.layout.action_bar_tab);
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeButtonEnabled(true);
+        ab.setTitle("MAROON 5");
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.group_map, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.group_info, menu);
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_car_pal_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == android.R.id.home) {
+            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
+                mDrawerLayout.closeDrawer(mDrawerList);
+            } else {
+                mDrawerLayout.openDrawer(mDrawerList);
+            }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
